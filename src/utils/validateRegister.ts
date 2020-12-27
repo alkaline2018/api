e5a7433ce7b77d4c8492abcb094e5909dda0411b
alkaline2018 <43746090+alkaline2018@users.noreply.github.com>
2020-12-27 11:51:09 +0900

    before Switching to TypeORM 다시 시작한 wingsuit

---
+++ src/utils/validateRegister.ts
@@ -0,0 +1,41 @@
+import { UsernamePasswordInput } from "src/resolvers/UsernamePasswordInput"
+
+export const validateRegister = (options: UsernamePasswordInput) => {
+    if(options.username.length <= 2){
+        return [{
+                field: 'username',
+                message: "length must be greater than 2"
+            }]
+        
+    }
+    if(options.username.includes("@")){
+        return [{
+                field: 'username',
+                message: "invalid username"
+            }]
+        
+    }
+    if(!options.email.includes("@")){
+        return [{
+                field: 'email',
+                message: "invalid email"
+            }]
+        
+    }
+    if(options.email.length <= 2){
+        return [{
+                field: 'email',
+                message: "length must be greater than 2"
+            }]
+        
+    }
+    if(options.password.length <= 3){
+        return [{
+                field: 'password',
+                message: "length must be greater than 3"
+            }]
+        
+    }
+
+    return null
+}
